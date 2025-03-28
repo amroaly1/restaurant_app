6e32321c62f96b09294948fa4b1b8a87c4909ec3
amroaly1 <amrtamea12@gmail.com>
2025-03-28 11:25:09 +0200

    Drop files from .gitignore

--- lib/core/network/api_base_url.dart
+++
@@ -1,8 +0,0 @@
-abstract class ApiBaseUrl {
-  static const String baseUrl =
-      "https://nti-ecommerce-api-production-ee3b.up.railway.app/api/";
-
-  // "https://nti-ecommerce-api-production.up.railway.app/api/";
-
-  // https://nti-ecommerce-api-production.up.railway.app/api/
-}
