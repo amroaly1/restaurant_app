02e6bbf072c8b4b141c520de9c9eadecfe4ddca3
amroaly1 <amrtamea12@gmail.com>
2025-03-28 11:26:19 +0200

    Drop files from .gitignore

---
+++ lib/core/network/api_end_point.dart
@@ -0,0 +1,17 @@
+abstract class ApiEndPoint {
+  static const String login = "login";
+  static const String register = "register";
+  static const String refreshtoken = "refresh_token";
+  static const String updateprofile = "update_profile";
+  static const String getuserdata = "get_user_data";
+  static const String deleteuser = "delete_user";
+  static const String orders = "orders";
+  static const String cancelorder = "orders/cancel";
+  static const String sliders = "sliders";
+  static const String bestsellerproducts = "best_seller_products";
+  static const String topratedproducts = "top_rated_products";
+  static const String addtofavorite = "add_to_favorite";
+  static const String categories = "categories";
+  static const String placeorder = "place_order";
+  static const String search = "products/search";
+}
