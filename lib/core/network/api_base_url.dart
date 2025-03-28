d5dc8c246f1efde6fa8d4cdc9e41b12ae8a841a0
amroaly1 <amrtamea12@gmail.com>
2025-03-28 11:19:53 +0200

    ignore 1

--- lib/core/network/api_base_url.dart
+++ lib/core/network/api_base_url.dart
@@ -1,8 +1,7 @@
 abstract class ApiBaseUrl {
   static const String baseUrl =
       "https://nti-ecommerce-api-production-ee3b.up.railway.app/api/";
-  static const String bb =
-      "https://nti-ecommerce-api-production-ee3b.up.railway.app/api/";
+
   // "https://nti-ecommerce-api-production.up.railway.app/api/";
 
   // https://nti-ecommerce-api-production.up.railway.app/api/
